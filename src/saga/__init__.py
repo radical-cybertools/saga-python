67c23339b68e4b264caf595fc6f9ce8dccea9557
Andre Merzky <andre@merzky.net>
2019-04-10 10:03:14 +0200

    add deprecation warning

--- src/saga/__init__.py
+++ src/saga/__init__.py
@@ -1,3 +1,30 @@
 
+import sys
+
+
+_msg = '''
+
+--------------------------------------------------------------------------------
+    WARNING: saga-python is deprecated!
+
+    Your application will still work - but "saga-python" has been renamed to
+    "radical.saga".  Please change your imports from
+
+        import saga
+
+    to
+
+        import radical.saga as saga
+
+    No other changes are needed.  This backward compatibility package will
+    disappear in version 1.0, planned for end 2019.
+--------------------------------------------------------------------------------
+
+'''
+
+sys.stderr.write(_msg)
+sys.stderr.flush()
+
+
 from radical.saga import *
 
